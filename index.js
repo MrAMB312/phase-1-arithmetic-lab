930d20a4264d98284ea44b21c2e2c089299ae3af
MrAMB312 <a.blomarz@gmail.com>
2024-09-26 20:43:28 -0500

    phase 1 arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+let multiply;
 
-// Write your code here
+let num1 = 31;
+
+let num2 = 2;
+
+multiply = num1 * num2;
+
+let random;
+
+random = Math.floor(Math.random() * 10) + 1;
+
+let mod;
+
+let num3 = 29;
+
+let num4 = 5;
+
+mod = num3 % num4;
+
+let max;
+
+max = Math.max(1, 4, 8, 9, 12, 15, 19, 20);
